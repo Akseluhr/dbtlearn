d1e7f52470fd51bab68d3505d346f8558a6f130d
Aksel Uhr <akseluhr8@gmail.com>
2024-11-06 15:59:00 +0100

    modify srtc_reviews

--- models/src/src_reviews.sql
+++ models/src/src_reviews.sql
@@ -5,8 +5,11 @@ WITH raw_reviews AS (
         AIRBNB.RAW.RAW_REVIEWS
 )
 SELECT
+    listing_id,
     date AS review_date,
+    reviewer_name,
     comments AS review_text,
     sentiment AS review_sentiment,
+
 FROM
     raw_reviews
